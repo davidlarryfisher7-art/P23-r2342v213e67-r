df426e15e8bf8b289bc60304c989e998ca58a13e
davidlarryfisher7-art <davidlarryfisher7@gmail.com>
2026-03-24 05:23:30 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'prver-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Home',
+  '/services',
+  '/work',
+  '/about',
+  '/ai-agent',
+  '/products',
+  '/blog',
+  '/privacy',
+  '/img/Ico.png',
+  '/img/logolight.png',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Cached Content
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
